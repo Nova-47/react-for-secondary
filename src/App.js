a48df50a4eb42b9d0d44cf9cd7956949ab96c0d9
Nova <bushdog094@gmail.com>
2025-01-23 05:06:33 +0900

    Initial commit with updated line endings

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time! haha!");
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <Button onClick={onClick} text={"click me"} />
+    </div>
+  );
+}
+
+export default App;
